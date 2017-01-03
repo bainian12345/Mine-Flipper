cf4e7a97268ea8b9e27520e234e840ae09f96a27
bainian12345 <bainian12345@hotmail.com>
2017-01-03 17:29:22 -0500

    fix location bug upon page refresh
    checks location to take user to last visited page upon refresh

--- www/js/mainController.js
+++ www/js/mainController.js
@@ -1,13 +1,29 @@
 app.controller('mainController', function($scope, $location) {
-	$scope.switchPage = function($event, page) {
+	var path = $location.path().replace("/", "");
+
+	function disablePages() {
 		for (var i = 0; i < $scope.pages.length; i++) {
 			$scope.pages[i].active = false;
-		}
+		}		
+	}
+
+	$scope.switchPage = function($event, page) {
+		disablePages();
 		page.active = true;
 	};
+
 	$scope.pages = [
 		{"name": "Play", "href": "/", "active": true},
 		{"name": "Vs. AI", "href": "/#/ai", "active": false},
 		{"name": "Rules", "href": "/#/rules", "active": false}
 	];
+
+	if (path === "ai") {
+		disablePages();
+		$scope.pages[1].active = true;
+	}
+	if (path === "rules") {
+		disablePages();
+		$scope.pages[2].active = true;
+	}
 });
