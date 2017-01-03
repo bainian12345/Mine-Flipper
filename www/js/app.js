6f6601d0d1b3f72e031c0f6d4cbb35cea236de58
bainian12345 <bainian12345@hotmail.com>
2017-01-03 17:37:49 -0500

    merge app.js and config.js
    moved routing into app.js

--- www/js/app.js
+++ www/js/app.js
@@ -2,18 +2,36 @@ var app = angular.module("app", ['ionic'])
       .run(function($ionicPlatform) {
         $ionicPlatform.ready(function() {
           if(window.cordova && window.cordova.plugins.Keyboard) {
-          // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-          // for form inputs)
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-          // Don't remove this line unless you know what you are doing. It stops the viewport
-          // from snapping when text inputs are focused. Ionic handles this internally for
-          // a much nicer keyboard experience.
-          cordova.plugins.Keyboard.disableScroll(true);
-        }
-        if(window.StatusBar) {
-          StatusBar.styleDefault();
-        }
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+          }
+          if(window.StatusBar) {
+            StatusBar.styleDefault();
+          }
+        });
+      })
+      .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+          .state('/', {
+              url: '/',
+              templateUrl: '../views/play.html',
+              controller: 'playController'
+          })
+          .state('ai', {
+              url: '/ai',
+              templateUrl: '../views/play.html',
+              controller: 'aiController'
+          })
+          .state('rules', {
+              url: '/rules',
+              templateUrl: '../views/rules.html',
+          });
+          $urlRouterProvider.otherwise('/');
       });
-    });
 
